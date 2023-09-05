888af652a888c8f106694f3c1bace76b5bd25806
Sefinek <sefinek5916@gmail.com>
2023-09-05 03:27:43 +0200

    Main commit

--- middlewares/morgan.js
+++ middlewares/morgan.js
@@ -1,17 +1,12 @@
 const morgan = require('morgan');
 
-const skipUserAgents = [
-	'Mozilla/5.0+(compatible; UptimeRobot/2.0; http://www.uptimerobot.com/)',
-	'updown.io daemon 2.9',
-];
+// const skipUserAgents = [];
 
-const skipUserAgent = () => req => {
-	const userAgent = req.headers['user-agent'];
-	return skipUserAgents.includes(userAgent);
-};
+// const skipUserAgent = () => req => {
+// 	const userAgent = req.headers['user-agent'];
+// 	return skipUserAgents.includes(userAgent);
+// };
 
-const logger = morgan('[:status :method :response-time ms] :url - :user-agent', {
-	skip: skipUserAgent(),
-});
+const logger = morgan('[:status :method :response-time ms] :url - :user-agent' /* { skip: skipUserAgent() } */);
 
 module.exports = logger;
